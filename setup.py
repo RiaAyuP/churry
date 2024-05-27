9cdbbee56aa6c1be0dd445759cf5ef34b960a208
RiaAyuP <mail.riaayupramudita@gmail.com>
2024-05-28 04:00:49 +0700

    added requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Organic Chemistry Chatbot',
+    version= '0.0.0',
+    author= 'Ria Ayu Pramudita',
+    author_email= 'contact@riaayupramudita.id',
+    packages= find_packages(),
+    install_requires = []
+)
