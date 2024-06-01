39a534df73f14f65715c052aac73e086ecf0ef98
RiaAyuP <mail.riaayupramudita@gmail.com>
2024-06-01 10:13:07 +0700

    updated helper.py

--- src/helper.py
+++ src/helper.py
@@ -8,12 +8,14 @@ OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 # load documents
 def load_pdf(data):
-    loader = DirectoryLoader(data, glob=".pdf", loader_cls=PyPDFLoader)
+    loader = DirectoryLoader(data, glob="*.pdf", loader_cls=PyPDFLoader)
     documents = loader.load()
+    print(f"Loaded {len(documents)} documents")
     return documents
 
 # split documents
 def text_split(extracted_data):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 150)
     text_chunks = text_splitter.split_documents(extracted_data)
+    print(f"Generated {len(text_chunks)} text chunks")
     return text_chunks
